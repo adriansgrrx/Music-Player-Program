74c002d095676587708c16c96551dc311dc86cce
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-02-11 07:09:55 +0800

    Removed the dictionary, instead used the Song object for storing default songs.

--- music_player.py
+++ music_player.py
@@ -1,69 +1,5 @@
 import collections
 
-playlist = {
-    "Flowers": {
-        "title": "Flowers",
-        "artist": "Miley Cyrus", 
-        "duration": 200 
-    },
-
-    "Kill Bill": {
-        "title": "Kill Bill",
-        "artist": "Sza", 
-        "duration": 154 
-    },
-
-    "Creepin'": {
-        "title": "Creepin'",
-        "artist": "The Weeknd & 21 Savage", 
-        "duration": 222
-    },
-    
-
-    "As It Was": {
-        "title": "As It Was",
-        "artist": "Harry Styles", 
-        "duration": 167
-    },
-
-    "Unholy": {
-        "title": "Unholy",
-        "artist": "Sam Smith", 
-        "duration": 157
-    },
-
-    "golden hour": {
-        "title": "golden hour",
-        "artist": "JVKE", 
-        "duration": 209
-    },
-
-    "Anti-Hero": {
-        "title": "Anti-Hero",
-        "artist": "Taylor Swift", 
-        "duration": 201
-    },
-
-    "Here With Me": {
-        "title": "Here With Me",
-        "artist": "d4vid", 
-        "duration": 242
-    },
-
-    "Made You Look": {
-        "title": "Made You Look",
-        "artist": "Megan Trainor", 
-        "duration": 134
-    },
-
-    "Lavander Haze": {
-        "title": "Lavander Haze",
-        "artist": "Taylor Swift", 
-        "duration": 202
-    },
-
-}
-
 class Song:
     def __init__(self, title, artist, duration):
         self.title = title
@@ -91,9 +27,18 @@ class MusicPlayer:
 player = MusicPlayer()
 
 # Add some songs to the library
-player.add_to_library(Song(playlist["Flowers"]["title"], playlist["Flowers"]["artist"], playlist["Flowers"]["duration"]))
-player.add_to_library(Song(playlist["Kill Bill"]["title"], playlist["Kill Bill"]["artist"], playlist["Kill Bill"]["duration"]))
-player.add_to_library(Song(playlist["Creepin'"]["title"], playlist["Creepin'"]["artist"], playlist["Creepin'"]["duration"]))
+yourSongs = [
+    Song("Flowers", "Miley Cyrus", 200),
+    Song("Kill Bill", "Sza", 154),
+    Song("Creepin'", "The Weeknd", 222),
+    Song("As it Was", "Harry Styles", 167),
+    Song("Unholy", "Sam Smith", 157),
+    Song("golden hour", "JVKE",  209),
+    Song("Anti-Hero", "Taylor Swift",  201),
+    Song("Here With Me", "d4vid",  242),
+    Song("Made You Look", "Megan Trainor",  134),
+    Song("Lavander Haze", "Taylor Swift",  202),
+]
 
 # Add songs to the queue
 player.song_queue.append(player.library[0])
