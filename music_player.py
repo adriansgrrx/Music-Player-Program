151ade80e7793fe49783d208de2bd484ce3bdd20
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-02-11 15:31:36 +0800

    imported msvcrt to handle next and previous song queue. Not fully polished.

--- music_player.py
+++ music_player.py
@@ -1,5 +1,6 @@
 import collections
 import time
+import msvcrt
 
 class Song:
     def __init__(self, title, artist, duration):
@@ -20,9 +21,18 @@ class MusicPlayer:
         while duration:
             mins, secs = divmod(duration, 60)
             timeFormat = '{:02d}:{:02d}'.format(mins, secs)
-            print(f"                                        {timeFormat}", end='\r')
+            print(f"                                    {timeFormat}", end='\r')
             time.sleep(1)
             duration -= 1
+            if msvcrt.kbhit():
+                key = msvcrt.getch().decode("utf-8")
+                if key == "N":
+                    player.playNextSong()
+                if key == "P":
+                    player.playPrevSong()
+            if duration == 0:
+                print("Goodbye!")
+                break
 
     def addToPlaylist(self, song):
         self.playlist.append(song)
@@ -30,31 +40,44 @@ class MusicPlayer:
     def searchSong(self, title):
         for song in self.playlist:
             if song.title == title:
-                print(f"                        Now playing: {song.title} by {song.artist}")
+                print(f"\n                      Now playing: {song.title} by {song.artist}")
+                player.durationCountdown(song.duration)
                 self.history.append(song)
                 return song
-        return print("                        The song is not on your playlist.")
+        return print("\n                    The song is not on your playlist.")
 
     def playAllSongs(self):
         for song in player.playlist:
-            print(f"                        Now playing, {song.title} by {song.artist}")
+            print(f"\n                      Now playing, {song.title} by {song.artist}")
             player.durationCountdown(song.duration)
+
+    def playSingleSong(self, title):
+        song = self.searchSong(title)
+        if song:
+            self.songQueue.append(song)
+            self.playNextSong()
+        else:
+            print("Song not found in the library.")
 
     def playNextSong(self):
         if not self.songQueue:
-            print("                        No songs in the queue.")
+            print("\n                           No songs in the queue.")
         else:
             song = self.songQueue.popleft()
             self.history.append(song)
-            print(f"                        Next song playing: {song.title} by {song.artist}")
+            print(f"\n                      Next song playing: {song.title} by {song.artist}")
+            player.durationCountdown(song.duration)
             
     def playPrevSong(self):
         if not self.history:
-            print("                        No previous songs in the playlist.")
+            print("\n                      No previous songs in the playlist.")
+        
         else:
             song = self.history.pop()
             self.songQueue.appendleft(song)
-            print(f"                        Previous song playing: {song.title} by {song.artist}")
+            print(f"\n                      Previous song playing: {song.title} by {song.artist}")
+            player.durationCountdown(song.duration)
+
 
 player = MusicPlayer()
 
@@ -105,10 +128,18 @@ def main():
     welcomeUser()
     for text in textsToBeCentered:
         centerOutput(text)
-    commands = int(input("\nHere are the commands:\n1. Play all\n2. Play a song\n3. Search a song\n>>> "))
+    commands = int(input("\nHere are the commands:\n1. Play all\n2. Play a song\n3. Search a song\n4. Add a song\n>>> "))
+
     if commands == 1:
         player.playAllSongs()
+    
+    elif commands == 2:
+        selectedSong = str(input("Song title: "))
+        player.playSingleSong(selectedSong)
 
+    elif commands == 3:
+        selectedSong = str(input("Song title: "))
+        player.searchSong(selectedSong)
 
 main()
     
